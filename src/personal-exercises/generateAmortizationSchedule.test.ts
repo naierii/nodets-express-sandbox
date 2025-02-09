8b69bc067360b6f96aad9bf341a0efd36e8ffb50
Reian Viloria <54141785+naierii@users.noreply.github.com>
2025-02-09 10:00:17 +0800

    feat: generate amortization schedule

--- src/personal-exercises/generateAmortizationSchedule.test.ts
+++ src/personal-exercises/generateAmortizationSchedule.test.ts
@@ -1,5 +1,6 @@
 import generateAmortizationSchedule, {
   calculateMonthlyPayment,
+  calculatePrincipalPayment,
 } from "./generateAmortizationSchedule";
 
 describe("Loan Amortization Schedule", () => {
@@ -7,7 +8,7 @@ describe("Loan Amortization Schedule", () => {
     const expectedResult = 536.82;
 
     const loanAmount = 100000;
-    const interest = 0.5;
+    const interest = 0.05;
     const loanTerm = 30;
 
     const calculatedMonthlyPayment = calculateMonthlyPayment(
@@ -16,56 +17,77 @@ describe("Loan Amortization Schedule", () => {
       loanTerm
     );
 
-    expect(calculatedMonthlyPayment).toBe(expectedResult);
+    expect(Math.round(calculatedMonthlyPayment * 100) / 100).toBe(
+      expectedResult
+    );
+  });
+
+  it("Should return a proper Principal Payment calculation for the Period 1", () => {
+    const expectedValue = 120.15;
+
+    const interestRate = 0.05;
+    const outstandingLoanBalance = 100000;
+    // I might wanna use a function on this one.
+    const totalMonthlyPayment = 536.82;
+
+    const calculatedPrincipalPayment = calculatePrincipalPayment({
+      interestRate,
+      outstandingLoanBalance,
+      totalMonthlyPayment,
+    });
+
+    expect(Math.round(calculatedPrincipalPayment * 100) / 100).toBe(
+      expectedValue
+    );
+  });
+
+  it("Should return a proper Principal Payment calculation for the Period 2", () => {
+    const expectedValue = 120.65;
+
+    const interestRate = 0.05;
+    const outstandingLoanBalance = 99879.85;
+    // I might wanna use a function on this one.
+    const totalMonthlyPayment = 536.82;
+
+    const calculatedPrincipalPayment = calculatePrincipalPayment({
+      interestRate,
+      outstandingLoanBalance,
+      totalMonthlyPayment,
+    });
+
+    expect(Math.round(calculatedPrincipalPayment * 100) / 100).toBe(
+      expectedValue
+    );
   });
 
   it("Should return loan data for a $100,000 at 5% interest for 30 years", () => {
     const expectedOutput = [
       {
-        period: 1,
-        /**
-         * i === <interest>
-         * n === <years> * <months>
-         *
-         * payment === <loan amount> * (
-         *  i * ((1 + i)**n)
-         *  /
-         *  ((1 + i)**n) - 1
-         * )
-         *
-         *
-         */
-        payment: 536.82,
-        interest: 416.67,
-        /**
-         * principalPayment === payment - (balance * (0.5 / (30 * 12)))
-         */
-        principalPayment: 120.15,
-        balance: 99879.85,
+        period: 1, // correct
+        payment: 536.82, // Correct, but round it
+        interest: 416.67, // To be corrected
+        principalPayment: 120.15, // Correct, but round it
+        balance: 99879.85, // Correct, but round it
       },
-      {
-        /**
-         * Period will always be +1 throughout the whole 30 years
-         */
-        period: 2,
-        /**
-         * The same as before
-         */
-        payment: 536.82,
 
+      {
+        period: 2,
+        payment: 536.82,
         interest: 416.17,
-        /**
-         * Why is the principal payment higher than before
-         */
         principalPayment: 120.65,
-        /**
-         * Balance became (balance - (<current principalPayment> + <prev principalPayment>))
-         */
         balance: 99759.2,
       },
+
       //... more periods...
     ];
 
     const schedule = generateAmortizationSchedule(100000, 0.05, 30);
+
+    expect(schedule[0]).toStrictEqual(expectedOutput[0]);
+    expect(schedule[1]).toStrictEqual(expectedOutput[1]);
+  });
+
+  it("Should return loan data for a $50,000 at 3% interest for 15 years", () => {
+    console.warn("Test case has no data");
   });
 });
--- src/personal-exercises/generateAmortizationSchedule.ts
+++ src/personal-exercises/generateAmortizationSchedule.ts
@@ -18,13 +18,14 @@ interface GenerateAmortizationSchedule {
  * )
  */
 export function calculateMonthlyPayment(
+  // TODO - make this an object
   loanAmount: number,
   interest: number,
   loanTerm: number
 ): number {
   // 12 represents the number of months in a year
   const numberOfPayments = loanTerm * 12;
-  const monthlyInterestPayment = interest / numberOfPayments;
+  const monthlyInterestPayment = interest / 12;
 
   // prettier-ignore
   const payment =
@@ -36,15 +37,72 @@ export function calculateMonthlyPayment(
   return payment;
 }
 
+// TODO - comment the formula and source
+interface CalculatePrincipalPayment {
+  interestRate: number;
+  outstandingLoanBalance: number;
+  totalMonthlyPayment: number;
+}
+export function calculatePrincipalPayment({
+  interestRate,
+  outstandingLoanBalance, // OLB
+  totalMonthlyPayment, // TMP
+}: CalculatePrincipalPayment): number {
+  // prettier-ignore
+  const principalPayment = totalMonthlyPayment - 
+  (
+    outstandingLoanBalance * (interestRate / 12)
+  );
+
+  return principalPayment;
+}
+
 export default function (
   principal: number, // I might wanna make this available for strings. eg. "100,000"
   interestRate: number,
   loanTerm: number
 ): GenerateAmortizationSchedule[] {
-  // Create a function for calculating payment
-  // Create a function that calculates principalPayment
+  let outstandingLoanBalance = principal;
+  const generatedAmortizationSchedule = [];
 
-  // Remove expect error once placeholder is removed
-  // @ts-expect-error
-  return "PLACEHOLDER";
+  let calculatedMonthlyPayment = calculateMonthlyPayment(
+    principal,
+    interestRate,
+    loanTerm
+  );
+  calculatedMonthlyPayment = roundedDecimal(calculatedMonthlyPayment, 2);
+
+  // This loop will calculate the monthly amortization schedule
+  // Loop this in loanTerm * 12
+  // iPeriod represents period of payment - 1 (index adjustment)
+  for (let iPeriod = 0; iPeriod < loanTerm * 12; iPeriod++) {
+    let calculatedPrincipalPayment = calculatePrincipalPayment({
+      interestRate,
+      outstandingLoanBalance,
+      totalMonthlyPayment: calculatedMonthlyPayment,
+    });
+    calculatedPrincipalPayment = roundedDecimal(calculatedPrincipalPayment, 2);
+
+    outstandingLoanBalance -= calculatedPrincipalPayment;
+
+    generatedAmortizationSchedule.push({
+      period: iPeriod + 1,
+      payment: calculatedMonthlyPayment,
+      principalPayment: calculatedPrincipalPayment,
+      interest: roundedDecimal(
+        calculatedMonthlyPayment - calculatedPrincipalPayment,
+        2
+      ),
+      balance: roundedDecimal(outstandingLoanBalance, 2),
+    });
+  }
+
+  return generatedAmortizationSchedule;
+}
+
+// TODO - relocate to number.util.ts
+function roundedDecimal(originalValue: number, position: number) {
+  // 1, 10, 100, etc,...
+  const positionPoint = +(1 + "0".repeat(position));
+  return Math.round(originalValue * positionPoint) / positionPoint;
 }
